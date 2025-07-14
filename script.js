f57d27ff6550291e1593fd3ae8978a50db6cf1c7
Santiago Cardona Herrera <santiago.cardona-herrera@flyr.com>
2025-07-14 08:35:46 -0500

    arreglo

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ function renderTodayEquipment() {
                 todayEquipments.push({
                     ...item,
                     office: officeNum
-                });
+                });         
             }
         });
     });
